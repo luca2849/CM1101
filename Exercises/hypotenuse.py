a770cb3d57a389b894d0de5c4c5a66a0a59f07ec
Luca Passariello <=>
2018-10-13 12:05:36 +0100

    Added validation to hypotenuse.py

--- Exercises/hypotenuse.py
+++ Exercises/hypotenuse.py
@@ -2,8 +2,29 @@ import math
 def calculate_hypotenuse(base, height):
     hypot = math.sqrt((base**2)+(height**2))
     return "\n" + str(round(hypot, 2))
-print("Welcome to the hypotenuse calculator")
-base = int(input("Please enter the base length of the triangle > "))
-height = int(input("Please enter the height of the triangle > "))
 
-print(calculate_hypotenuse(base, height))
+def is_float(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
+print("Welcome to the hypotenuse calculator")
+
+base = input("Please enter the base length of the triangle > ")
+
+while is_float(base) != True:
+    print("Input must be numeric")
+    base = input("Please enter the base length of the triangle > ")
+
+
+height = input("Please enter the height of the triangle > ")
+height_type = type(height)
+while is_float(height) != True:
+    print("Input must be numeric")
+    height = input("Please enter the height of the triangle > ")
+
+
+
+print(calculate_hypotenuse(float(base), float(height)))
